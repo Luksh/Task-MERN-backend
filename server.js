cea2617ea399e51654acf645a37bf16677a3a856
Laxman Maharjan <hotmale_luksh@hotmail.com>
2024-03-02 08:58:23 +0545

    User Registration

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/tasks", require("./Routes/taskRoutes"));
+app.use("/api/users", require("./Routes/userRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => {
