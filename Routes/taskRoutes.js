6fd5075336fa2b81f629ae679ba337c455fdfaf0
Laxman Maharjan <hotmale_luksh@hotmail.com>
2024-03-03 17:20:49 +0545

    Auth Middleware and Protect Routes

--- Routes/taskRoutes.js
+++ Routes/taskRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { getTasks, setTask, updateTask, deleteTask } = require("../Controllers/taskController");
 const router = express.Router();
+const { protect } = require("../Middleware/authMiddleware");
 
-router.get("/", getTasks);
-router.post("/", setTask);
-router.put("/:id", updateTask);
-router.delete("/:id", deleteTask);
+router.get("/", protect, getTasks);
+router.post("/", protect, setTask);
+router.put("/:id", protect, updateTask);
+router.delete("/:id", protect, deleteTask);
 
 module.exports = router;
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser, getCurrentUser } = require("../Controllers/userController");
+const { protect } = require("../Middleware/authMiddleware");
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
-router.get("/current", getCurrentUser);
+router.get("/current", protect, getCurrentUser);
 
 module.exports = router;
